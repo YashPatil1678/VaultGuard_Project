d9472d5423298f1dd3f4ade16fa3efcc7a90a256
YashPatil1678 <yashp1678@gmail.com>
2025-09-21 16:54:34 +0530

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+locals {
+  region = "ap-south-1"
+  name   = "VaultGuard-cluster"
+  vpc_cidr = "10.0.0.0/16"
+  azs      = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.0.0/20", "10.0.16.0/20"]
+  private_subnets = ["10.0.32.0/19", "10.0.64.0/19"]
+  tags = {
+    Example = local.name
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
