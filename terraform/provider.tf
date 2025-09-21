93fd3767e936707a90b2fe99f57d667e0183e105
YashPatil1678 <yashp1678@gmail.com>
2025-09-21 17:26:27 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,6 +11,17 @@ locals {
   }
 }
 
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
 provider "aws" {
   region = "ap-south-1"
 }
