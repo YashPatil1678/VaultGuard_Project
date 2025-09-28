ab66bef775fc6e1420387613d1a1fe0131d4118b
YashPatil1678 <yashp1678@gmail.com>
2025-09-28 16:17:29 +0530

    Update alb_controller_attach.tf

--- terraform/alb_controller_attach.tf
+++ terraform/alb_controller_attach.tf
@@ -1,4 +1,9 @@
 resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
   role       = aws_iam_role.alb_ingress.name
   policy_arn = aws_iam_policy.alb_controller.arn
+
+  depends_on = [
+    aws_iam_role.alb_ingress,
+    aws_iam_policy.alb_controller
+  ]
 }
