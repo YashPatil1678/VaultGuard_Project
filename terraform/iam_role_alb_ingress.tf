388ebc6e638738e1767058811e033c9ca08cc360
YashPatil1678 <yashp1678@gmail.com>
2025-09-28 14:47:44 +0530

    Create iam_role_alb_ingress.tf

---
+++ terraform/iam_role_alb_ingress.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "alb_ingress" {
+  name = "AmazonEKSLoadBalancerControllerRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(data.aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      }
+    ]
+  })
+}
