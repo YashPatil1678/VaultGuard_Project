862f27de23bb4f25f25dd821f7dfc8cfa137bc11
YashPatil1678 <yashp1678@gmail.com>
2025-09-28 17:20:01 +0530

    Update alb_controller_attach.tf

--- terraform/alb_controller_attach.tf
+++ terraform/alb_controller_attach.tf
@@ -1,9 +1,8 @@
 resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
   role       = aws_iam_role.alb_ingress.name
-  policy_arn = aws_iam_policy.alb_controller.arn
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancerControllerPolicy"
 
   depends_on = [
-    aws_iam_role.alb_ingress,
-    aws_iam_policy.alb_controller
+    aws_iam_role.alb_ingress
   ]
 }
