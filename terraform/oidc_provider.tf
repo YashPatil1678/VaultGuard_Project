a63f4fdc5c1c090e357d618b29e18d6efcdd22cb
YashPatil1678 <yashp1678@gmail.com>
2025-09-28 14:42:25 +0530

    Create oidc_provider.tf

---
+++ terraform/oidc_provider.tf
@@ -0,0 +1,20 @@
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+data "tls_certificate" "oidc_thumbprint" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
+  url             = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+
+  depends_on = [module.eks]   # Ensures EKS cluster is created first
+}
+
