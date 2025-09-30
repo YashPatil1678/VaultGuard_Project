297dfedacbfee1397d1273033c9f4dc0f376bbbe
YashPatil1678 <yashp1678@gmail.com>
2025-09-30 18:31:27 +0530

    Delete terraform/outputs.tf

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_controller_role_arn" {
-  description = "ARN of the IAM role for ALB ingress controller"
-  value       = aws_iam_role.alb_ingress.arn
-}
-
-output "cluster_name" {
-  description = "The name of the EKS cluster"
-  value = module.eks.cluster_name
-}
