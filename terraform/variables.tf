879168a3b39b4d341da078fa87abf9a97d3fa18c
YashPatil1678 <yashp1678@gmail.com>
2025-09-30 18:33:06 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "VaultGuard-cluster"
+}
+
+variable "owner" {
+  description = "Owner tag"
+  type        = string
+  default     = "yash_patil"
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
