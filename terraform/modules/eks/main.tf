c12c3dab1613aaa41a783ab5150e91c1b02b45b3
YashPatil1678 <yashp1678@gmail.com>
2025-09-30 20:37:13 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,19 +1,19 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "19.15.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.26"
-  subnets        = var.private_subnets
-  vpc_id         = var.vpc_id
+  subnet_ids      = var.private_subnets
+  vpc_id          = var.vpc_id
 
-  node_groups = {
+  eks_managed_node_groups = {
     default = {
-      desired_capacity = var.node_desired_capacity
-      min_capacity     = var.node_min_capacity
-      max_capacity     = var.node_max_capacity
-      instance_types   = var.node_instance_types
-      key_name         = var.key_name
+      desired_size = var.node_desired_capacity
+      min_size     = var.node_min_capacity
+      max_size     = var.node_max_capacity
+      instance_types = var.node_instance_types
+      key_name     = var.key_name
     }
   }
 
