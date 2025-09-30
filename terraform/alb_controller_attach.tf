edcf931e41c01ec0468b63b313be0491b448c7aa
YashPatil1678 <yashp1678@gmail.com>
2025-09-30 17:44:35 +0530

    Update alb_controller_attach.tf

--- terraform/alb_controller_attach.tf
+++ terraform/alb_controller_attach.tf
@@ -1,8 +1,22 @@
-resource "aws_iam_role_policy_attachment" "alb_controller_attach" { 
-  role = aws_iam_role.alb_ingress.name 
-  policy_arn = aws_iam_policy.alb_controller.arn 
-  
-  depends_on = [ 
+# Attach custom ALB controller policy
+resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
+  role       = aws_iam_role.alb_ingress.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+
+depends_on = [ 
     aws_iam_role.alb_ingress, aws_iam_policy.alb_controller
   ]
 }
+
+# Attach AWS-managed ELB full access policy
+resource "aws_iam_role_policy_attachment" "alb_controller_elb_attach" {
+  role       = aws_iam_role.alb_ingress.name
+  policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
+
+depends_on = [ 
+    aws_iam_role.alb_ingress, aws_iam_policy.alb_controller
+  ]
+}
+
+  
+  
