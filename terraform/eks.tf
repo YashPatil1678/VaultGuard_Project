15e5f158ba9e107de233fd314db4ecbdbfe83bb7
YashPatil1678 <yashp1678@gmail.com>
2025-10-02 18:07:24 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,24 +1,45 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"         # module version - only once here
+  version = "19.15.1"
 
-  name            = local.name
-  cluster_version = "1.33"    # Kubernetes version for your cluster
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
 
-  vpc_id           = module.vpc.vpc_id
-  subnet_ids       = module.vpc.private_subnets
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
-  public_access_cidrs = ["0.0.0.0/0"]  # to enable public access
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
-  node_groups = {
-    default = {
-      desired_capacity = 1
-      max_capacity     = 2
-      min_capacity     = 1
-      instance_types   = ["t2.micro"]
-      capacity_type    = "SPOT"
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["t2.micro"]
+
+    attach_cluster_primary_security_group = true
+  }
+
+  eks_managed_node_groups = {
+    vaultguard-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.micro"]
+      capacity_type  = "SPOT"
+
       tags = {
-        Name = "eks-node-group-default"
+        ExtraTag = "VaultGuard"
       }
     }
   }
