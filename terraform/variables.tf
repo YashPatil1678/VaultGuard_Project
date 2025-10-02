90223e5f4fa0420c172904b441a2165e2f43d29e
YashPatil1678 <yashp1678@gmail.com>
2025-10-02 15:38:37 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,29 +7,68 @@ variable "region" {
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
-  default     = "VaultGuard-cluster"
+  default     = "vaultguard-eks"
 }
 
-variable "owner" {
-  description = "Owner tag"
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
   type        = string
-  default     = "yash_patil"
+  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
-  description = "Availability zones"
+  description = "Availability zones to use"
   type        = list(string)
   default     = ["ap-south-1a", "ap-south-1b"]
 }
 
-variable "public_subnet_cidrs" {
+variable "public_subnets" {
   description = "Public subnet CIDRs"
   type        = list(string)
-  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
-variable "private_subnet_cidrs" {
+variable "private_subnets" {
   description = "Private subnet CIDRs"
   type        = list(string)
-  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for EKS"
+  type        = string
+  default     = "1.26"
+}
+
+variable "node_instance_types" {
+  description = "Instance types for the managed node group"
+  type        = list(string)
+  default     = ["t2.micro"]
+}
+
+variable "node_desired_capacity" {
+  description = "Desired node count"
+  type        = number
+  default     = 1
+}
+
+variable "node_min_capacity" {
+  description = "Minimum nodes"
+  type        = number
+  default     = 1
+}
+
+variable "node_max_capacity" {
+  description = "Maximum nodes"
+  type        = number
+  default     = 2
+}
+
+variable "tag_map" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {
+    Owner   = "yash"
+    Project = "VaultGuard"
+  }
 }
