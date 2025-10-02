c9e9a8423be66421c2a84ab01f510294126cdb26
YashPatil1678 <yashp1678@gmail.com>
2025-10-02 18:11:15 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,7 +10,7 @@ module "vpc" {
   public_subnets  = local.public_subnets
  
   enable_nat_gateway = true
-  ingle_nat_gateway  = true  
+  single_nat_gateway  = true  
 
   enable_dns_hostnames = true
   enable_dns_support   = true
