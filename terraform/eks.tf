f4acc1dd6340a8a12986e37dba49258a6b0ee297
YashPatil1678 <yashp1678@gmail.com>
2025-10-02 15:40:38 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,34 +1,33 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.0"
+  version = "20.8.5" # lock to a recent stable v20 release
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
-  vpc_id  = module.vpc.vpc_id
-  subnets = module.vpc.private_subnets
+  vpc_id      = module.vpc.vpc_id
+  subnet_ids  = module.vpc.private_subnets
 
-  node_groups = {
-    managed_nodes = {
-      desired_capacity = var.node_desired_capacity
-      min_capacity     = var.node_min_capacity
-      max_capacity     = var.node_max_capacity
+  eks_managed_node_groups = {
+    default = {
+      desired_size = var.node_desired_capacity
+      min_size     = var.node_min_capacity
+      max_size     = var.node_max_capacity
 
       instance_types = var.node_instance_types
       disk_size      = 10
 
-      additional_tags = {
+      labels = {
+        role = "worker"
+      }
+
+      tags = {
         "Name" = "${var.cluster_name}-node"
       }
     }
   }
 
-  manage_aws_auth = true
-
   tags = merge(var.tag_map, {
     "Name" = var.cluster_name
   })
-
-  write_kubeconfig = true
-  kubeconfig_path  = "${path.module}/kubeconfig_${var.cluster_name}"
 }
