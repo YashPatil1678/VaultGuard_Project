ad89fb18ceb7859b8cba3bc187d92145003fadd2
YashPatil1678 <yashp1678@gmail.com>
2025-10-02 18:01:41 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,56 +2,29 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  cluster_name                   = local.name
-  cluster_version                = "1.33"               # Specify your desired Kubernetes version
-  cluster_endpoint_public_access = true                 # Enable public access to API server
-  cluster_endpoint_private_access = false               # Disable private access (optional)
-
-  # You can restrict API access to specific CIDRs (default is 0.0.0.0/0)
-  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"] 
+  name    = local.name                   # Was cluster_name before
+  version = "1.33"                      # Kubernetes version
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets               # Subnets for EKS worker nodes
+  subnet_ids = module.vpc.private_subnets
 
-  # Optional: Specify the IAM role for cluster if you manage it separately
-  # cluster_iam_role_name = "eks-cluster-role"
+  # Public access configuration (note different name)
+  public_access_cidrs = ["0.0.0.0/0"]  # Allow all IPs public access
 
-  # Enable and configure cluster addons
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
-  # Defaults applied to all managed node groups
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["t2.micro"]
-    attach_cluster_primary_security_group = true
-  }
-
-  eks_managed_node_groups = {
+  # Managed node group(s)
+  node_groups = {
     default = {
-      desired_size = 1
-      max_size     = 2
-      min_size     = 1
-
-      instance_types = ["t2.micro"]
-      capacity_type  = "SPOT"
-
+      desired_capacity = 1
+      max_capacity     = 2
+      min_capacity     = 1
+      instance_types   = ["t2.micro"]
+      capacity_type    = "SPOT"
       tags = {
         Name = "eks-node-group-default"
       }
     }
   }
 
-  # Tags for the cluster and resources
   tags = {
     Environment = "dev"
     Terraform   = "true"
