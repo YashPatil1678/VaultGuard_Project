222db47ba73385daec0d39dbd9d54de925290270
YashPatil1678 <yashp1678@gmail.com>
2025-10-02 18:14:13 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,25 +1,25 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
+#output "vpc_id" {
+#  value = module.vpc.vpc_id
+#}
 
-output "private_subnets" {
-  value = module.vpc.private_subnets
-}
+#output "private_subnets" {
+#  value = module.vpc.private_subnets
+#}
 
-output "public_subnets" {
-  value = module.vpc.public_subnets
-}
+#output "public_subnets" {
+#  value = module.vpc.public_subnets
+#}
 
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
+#output "cluster_name" {
+#  value = module.eks.cluster_name
+#}
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+#output "cluster_endpoint" {
+#  value = module.eks.cluster_endpoint
+#}
 
-output "kubeconfig_file" {
-  value = "${path.module}/kubeconfig_${var.cluster_name}"
-}
+#output "kubeconfig_file" {
+#  value = "${path.module}/kubeconfig_${var.cluster_name}"
+#}
 
 
