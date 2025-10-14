62c83d8612556a1b3ae430b264667a101e5f4f0e
YashPatil1678 <yashp1678@gmail.com>
2025-10-14 17:22:37 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,26 +1,19 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  version = "~> 5.0" # Use the latest compatible version
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name = "${var.cluster_name}-vpc"
+  cidr = var.vpc_cidr
 
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
- 
+  azs             = [var.availability_zone_a, var.availability_zone_b]
+  public_subnets  = [var.public_subnet_a_cidr, var.public_subnet_b_cidr]
+  private_subnets = [var.private_subnet_a_cidr, var.private_subnet_b_cidr]
+
   enable_nat_gateway = true
-  single_nat_gateway  = true  
+  single_nat_gateway = true
 
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
+
